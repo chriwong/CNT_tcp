174fdef788e362fa44c099e5465a2f93e28daf99
chriwong <chriwong@ufl.edu>
2019-02-18 11:03:21 -0500

    Fixed file opening. Data now persists

--- Wong_Project1/client.py
+++ Wong_Project1/client.py
@@ -1,9 +1,39 @@
+import pickle
 import socket
 import sys
-import pickle
+
+class QuestionEntry:
+    """Questions have a tag, text, two or more choices, and an answer"""
+    def __init__(self):
+        self.tag = ''
+        self.text = ''
+        self.choices = {}   # {letter:choiceText} that way the answer is an index
+        self.answer = ''
+
+def getValidInput():
+    while True:
+        try:
+            s = str(input("> "))
+            # Error checking for c <n> <a> is just too much...
+            if s=='p' or s=='r' or s=='k' or s=='q' or s=='h' or s[0]=='c':
+                return s
+            else:
+                try:
+                    if ( (s[0]=='d' or s[0]=='g') and isinstance(int(s[2:]), int) ):
+                        return s
+                    else:
+                        print('Error: incorrect input. Choose pdgrckqh')
+                except IndexError as e:
+                    print('Error: missing arguments')
+                except ValueError as e:
+                    print('Error: numeric arguments expected')
+        except Exception as e:
+            print('Error:', str(e))
+            s = None
 
 def main():
 
+    # Need command-line args to connect
     if len(sys.argv) < 2:
         print('Error: missing command-line arguments', file=sys.stderr)
         sys.exit(-1)
@@ -11,29 +41,90 @@ def main():
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        clientSocket.connect((sys.argv[1], sys.argv[2]))
+        # Connect to address specified in command-line args
+        clientSocket.connect((sys.argv[1], int(sys.argv[2])))
         print('Connected!')
-                        
-        serverWelcome = clientSocket.recv(1024)
-        print('The server said: ', serverWelcome.decode())
-                        
-        clientWelcome = input('Say hello back: ')
-        clientSocket.send(clientWelcome.encode())
-                        
-        print('Closing connection...')
-        clientSocket.close()
-        
-    except ConnectionRefusedError e:
+
+
+        while True:
+            # Get menu choice and send to server
+            menuOption = getValidInput()
+            clientSocket.send(menuOption.encode())
+
+            if menuOption == 'p':
+                # Tag
+                tag = input()
+                clientSocket.send(tag.encode())
+
+                # Text
+                text = ''
+                buf = input()
+                while buf != '.':
+                    text += buf
+                    buf = input()
+                clientSocket.send(text.encode())
+
+                # Choices
+                # Send each choice, including letter, to server
+                # When choices are finished, send a period to indicate as such
+                choice = input()
+                while True:
+                    buf = input()
+                    while buf != '.':
+                        choice += '\n'  # Keep multi-line choices multi-line
+                        choice += buf
+                        buf = input()
+                    clientSocket.send(choice.encode())
+                    terminator = input()
+                    if terminator == '.':
+                        clientSocket.send(terminator.encode())
+                        break
+                    else:
+                        choice = terminator
+
+                # Answer
+                a = input()
+                clientSocket.send(a.encode())
+
+            elif menuOption[0] == 'd':
+                pass
+                # Delete
+
+            elif menuOption[0] == 'g':
+                pass
+                # Get
+
+            elif menuOption == 'r':
+                pass
+                # Random
+
+            elif menuOption[0] == 'c':
+                pass
+                # Answer
+
+            elif menuOption == 'k':
+                pass
+                # Kill server
+
+            elif menuOption == 'q':
+                # Kill client only
+                clientSocket.close()
+                sys.exit(1)
+
+            elif menuOption == 'h':
+                pass
+
+            else:
+                # This shouldn't be reachable
+                print('Error: unhandled input')
+
+    except ConnectionRefusedError as e:
         print('Error: unable to connect', file=sys.stderr)
         sys.exit(-1)
     except Exception as e:
         print('Error:', str(e), file=sys.stderr)
         sys.exit(-1)
         
-        
-
-    
-print('goodbye')
 
 if __name__ == '__main__':
 	main()
--- Wong_Project1/server.py
+++ Wong_Project1/server.py
@@ -1,28 +1,121 @@
+import os
+import random
+import pickle
 import socket
 import sys
-import pickle
+
+class QuestionEntry:
+    """Questions have a tag, text, two or more choices, and an answer"""
+    def __init__(self):
+        self.tag = ''
+        self.text = ''
+        self.choices = {}   # {letter:choiceText} that way the answer is an index
+        self.answer = ''
+
+QuestionBank = {}
+QuestionNumber = 0
+
+try:
+    if os.stat('questionbank.pickle').st_size != 0:
+        with open('questionbank.pickle', 'rb') as file:
+            QuestionBank = pickle.load(file)
+            file.close()
+    else:
+        pass
+
+except FileNotFoundError as e:
+    print('Creating new question bank in questionbank.pickle')
+    a = open('questionbank.pickle', 'wb+')
+    a.close()
+
+
 
 def main():
+    global QuestionBank, QuestionNumber
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     try:
-        serverSocket.bind('', 0)
+        # Get local address, begin listening, and report port obtained
+        serverSocket.bind(('', 0))
         serverSocket.listen(1)
         print(serverSocket.getsockname()[1])
 
+        # Loop to continue accepting connections
         while True:
-            connectionSocket, addr = serverSocket.accept()	# serverSocket keeps listening; connectionSocket can now communicate 
-            serverWelcome = 'Welcome to the Networking project'
-            connectionSocket.send(serverWelcome.encode())
-                            
-            clientWelcome = connectionSocket.recv(1024)
-            print('The client said: ', clientWelcome.decode())
+            connectionSocket, addr = serverSocket.accept()
+            print(' ~Got a new connection')
+
+            while True:
+                menuOption = connectionSocket.recv(1024).decode()
+                print('Menu option:', menuOption)
+
+                if menuOption == 'p':
+                    newEntry = QuestionEntry()
+                    newEntry.tag = connectionSocket.recv(1024).decode()
+                    newEntry.text = connectionSocket.recv(1024).decode()
+                    choice = connectionSocket.recv(1024).decode()
+                    while choice != '.':
+                        newEntry.choices[choice[1]] = choice[4:]
+                        choice = connectionSocket.recv(1024).decode()
+                    newEntry.answer = connectionSocket.recv(1024).decode()
+                    QuestionBank[QuestionNumber] = newEntry
+                    QuestionNumber += 1
+
+                elif menuOption[0] == 'd':
+                    pass
+                    # Delete
+
+                elif menuOption[0] == 'g':
+                    pass
+                    # Get
+
+                elif menuOption == 'r':
+                    pass
+                    # Random
+
+                elif menuOption[0] == 'c':
+                    pass
+                    # Answer
+
+                elif menuOption == 'k':
+                    # I'm too lazy to send message for confirmation
+                    print('Terminating...')
+                    sys.exit(1)
+
+                elif menuOption == 'q':
+                    print('Client initiated closing connection')
+                    break
+
+                elif menuOption == 'h':
+                    pass
+                    # Help
+                    # Server does nothing
+
+                else:
+                    # This shouldn't be reachable
+                    print('Error: unhandled input')
+
+                printQuestionBank()
+                with open('questionbank.pickle', 'wb+') as file:
+                    pickle.dump(QuestionBank, file)
                             
             connectionSocket.close()
-            print('Client left...')
+            print(' ~Closed connection')
             
     except Exception as e:
-        print('Error: unable to create server', file=sys.stderr)
+        print('Error:', str(e), file=sys.stderr)
         sys.exit(-1)
+
+def printQuestionBank():
+    global QuestionBank
+    for n, q in QuestionBank.items():
+        print('~Question ', n, '~', sep='')
+        print('Tag: ', q.tag, sep='')
+        print('Text: ', q.text, sep='')
+        print('Choices:')
+        for c, a in q.choices.items():
+            print('\t(', c, '): ', a, sep='')
+        print('Answer: ', q.answer)
 
 
 if __name__ == '__main__':
